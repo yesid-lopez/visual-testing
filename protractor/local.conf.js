a2bb777c9a8e46c3b60047f08debf8f8f188b9a8
Yesid Leonardo Lopez Sierra <ylopezs@psl.com.co>
2019-07-24 22:08:44 -0500

    delete pix diff

--- protractor/local.conf.js
+++ protractor/local.conf.js
@@ -1,20 +1,8 @@
-const PixDiff = require('pix-diff');
 exports.config = {
-    framework: 'jasmine2',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['../test/**/*.spec.js'],
-    capabilities: {
-      browserName: 'chrome'
-    },
-    onPrepare() {
-        PixDiff.loadMatchers();
-        browser.pixDiff = new PixDiff(
-          {
-            basePath: 'test/visual/resources/screenshots',
-            diffPath: 'test/visual/resources/screenshots',
-            formatImageName: '{tag}-{browserName}-{width}x{height}',
-            baseline: true
-          }
-        );
-      }
-  }
+  framework: 'jasmine2',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['../test/**/*.spec.js'],
+  capabilities: {
+    browserName: 'chrome'
+  },
+}
