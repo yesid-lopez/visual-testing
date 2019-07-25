7c7ba22ca9bf6fe71daa449ed6a36a0474c00a9f
Yesid Leonardo Lopez Sierra <ylopezs@psl.com.co>
2019-07-24 20:06:42 -0500

    add pix-diff config

--- protractor/local.conf.js
+++ protractor/local.conf.js
@@ -1,4 +1,19 @@
 exports.config = {
+    framework: 'jasmine2',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: '../test/**/*.spec.js'
-};
+    specs: ['../test/**/*.spec.js'],
+    capabilities: {
+      browserName: 'chrome'
+    },
+    onPrepare() {
+        const PixDiff = require('pix-diff');
+        PixDiff.loadMatchers();
+        browser.pixDiff = new PixDiff(
+          {
+            basePath: 'test/visual/resources/screenshots',
+            diffPath: 'test/visual/resources/screenshots',
+            formatImageName: '{tag}-{browserName}-{width}x{height}'
+          }
+        );
+      }
+  }
