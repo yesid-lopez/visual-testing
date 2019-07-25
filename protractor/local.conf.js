1e981bdfa0be06a8908dccedf07c65c7e4101bb8
Yesid Leonardo Lopez Sierra <ylopezs@psl.com.co>
2019-07-24 21:23:57 -0500

    add baseline parameter

--- protractor/local.conf.js
+++ protractor/local.conf.js
@@ -1,3 +1,4 @@
+const PixDiff = require('pix-diff');
 exports.config = {
     framework: 'jasmine2',
     seleniumAddress: 'http://localhost:4444/wd/hub',
@@ -6,13 +7,13 @@ exports.config = {
       browserName: 'chrome'
     },
     onPrepare() {
-        const PixDiff = require('pix-diff');
         PixDiff.loadMatchers();
         browser.pixDiff = new PixDiff(
           {
             basePath: 'test/visual/resources/screenshots',
             diffPath: 'test/visual/resources/screenshots',
-            formatImageName: '{tag}-{browserName}-{width}x{height}'
+            formatImageName: '{tag}-{browserName}-{width}x{height}',
+            baseline: true
           }
         );
       }
